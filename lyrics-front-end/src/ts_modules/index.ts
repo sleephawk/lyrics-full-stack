ca700b824584bc84a89da37025858628ba56a475
Matt <designs.sleephawk@gmail.com>
2025-08-14 09:55:13 +0100

    added front end to parse multiple songs and provide dynamic youtube link. Added Scss code to backup changes on front end

--- lyrics-front-end/src/ts_modules/index.ts
+++ lyrics-front-end/src/ts_modules/index.ts
@@ -1,57 +1,96 @@
 import "../style.scss";
+import { sanitiseInput } from "./utils";
 import { sleep } from "./utils";
 
 //Query Selectors
 
-const searchLyricSubmit = document.querySelector<HTMLButtonElement>("#searchLyricSubmit");
+const searchLyricSubmit =
+  document.querySelector<HTMLButtonElement>("#searchLyricSubmit");
 const displayArea = document.querySelector<HTMLDivElement>(".display-area");
-const responseDisplay = document.querySelector<HTMLParagraphElement>("#display-response");
-const quickMessage = document.querySelector<HTMLParagraphElement>(".display-area--quick-message");
+const responseDisplay =
+  document.querySelector<HTMLParagraphElement>("#display-response");
+const quickMessage = document.querySelector<HTMLParagraphElement>(
+  ".display-area--quick-message"
+);
 
-if (!searchLyricSubmit || !responseDisplay || !responseDisplay || !quickMessage || !displayArea) {
+if (
+  !searchLyricSubmit ||
+  !responseDisplay ||
+  !responseDisplay ||
+  !quickMessage ||
+  !displayArea
+) {
   throw new Error("Missing DOM elements on home page");
 }
 
-//UTILS
-const cleanInput = (input: string): string => {
-  return input.trim().replace(/\s+/g, " ");
-};
-
-//API Handling
 interface SongDetails {
   id: number;
-  song: string;
-  artist: string;
-  year: number;
-  lyrics: string;
-  genre: string;
+  name: string;
+  artistNames: string[];
+  releaseYear: number;
+  lyrics: string[];
+  genreNames: string[];
 }
 
-const searchSong = async (input: string): Promise<SongDetails> => {
-  const lyric: string = cleanInput(input);
-  const url: URL = new URL("http://localhost:8080/api/songs");
+const searchSong = async (input: string): Promise<SongDetails[]> => {
+  const lyric: string = sanitiseInput(input);
+  const url = new URL("http://localhost:8080/api/songs");
   url.searchParams.append("words", lyric);
+
   const response = await fetch(url.toString());
-  if (!response.ok) throw new Error("Could not find this song, are you sure you typed it correctly? It may also not be on our database");
-  const songDetails: SongDetails = await response.json();
+  if (!response.ok) {
+    throw new Error(
+      "Could not find this song, are you sure you typed it correctly? It may also not be on our database"
+    );
+  }
+
+  const songDetails: SongDetails[] = await response.json();
   return songDetails;
 };
+
 searchLyricSubmit.addEventListener("click", async (e) => {
   e.preventDefault();
-
-  const lyricForm = document.querySelector("#input-field") as HTMLTextAreaElement;
+  responseDisplay.innerHTML = "";
+  const lyricForm = document.querySelector(
+    "#input-field"
+  ) as HTMLTextAreaElement;
 
   try {
     const song = await searchSong(lyricForm.value);
 
-    console.log(song);
+    song.forEach(async (s, i) => {
+      let card = document.createElement("p");
+      card.classList.add("display-area--text__card");
+      card.innerHTML = `▶ ${s.artistNames.join(", ")}:<br> ${s.name} `;
+      let cardExpand = false;
+      responseDisplay.appendChild(card);
+      await sleep((i + 1) * 200);
+      card.style.opacity = "1";
+      card.addEventListener("click", () => {
+        const youtube: string = `https://www.youtube.com/results?search_query=${
+          s.name
+        }+${s.artistNames.join("+")}`;
 
-    responseDisplay.innerHTML = `
-    Song: ${song.song}<br><br>
-    Artist: ${song.artist}<br><br>
-    Year: ${song.year}<br><br>
-    Lyrics:\n${song.lyrics}<br><br>
-    Genre: ${song.genre}`;
+        if (!cardExpand) {
+          const link: HTMLAnchorElement = document.createElement("a");
+          link.setAttribute("href", youtube);
+          link.setAttribute("target", "_blank");
+          link.textContent = "Find on Youtube";
+          card.innerHTML = `
+            ▼ ${s.artistNames.join(", ")}<br><br>
+            ${s.name}<br><br>
+            Release Year: ${s.releaseYear}<br><br>
+            Genre(s): ${s.genreNames.join(", ")}<br><br>
+            Lyrics:<br> ${s.lyrics}<br><br>
+            `;
+          card.appendChild(link);
+          cardExpand = true;
+        } else {
+          card.innerHTML = `▶ ${s.artistNames.join(", ")}:<br> ${s.name} `;
+          cardExpand = false;
+        }
+      });
+    });
 
     displayArea.style.opacity = "1";
     quickMessage.style.opacity = "1";
@@ -98,3 +137,5 @@ searchLyricSubmit.addEventListener("click", async (e) => {
 /**
  * [{"id":2,"name":"Yellow Submarine","lyrics":"In the town where I was born Lived a man who sailed to sea And he told us of his life","releaseYear":1980,"authors":[{"id":3,"name":"Beetles"}],"genres":[{"id":3,"name":"Pop"},{"id":4,"name":"Brit Pop"}]}]
  */
+
+//http://localhost:8080/api/songs
