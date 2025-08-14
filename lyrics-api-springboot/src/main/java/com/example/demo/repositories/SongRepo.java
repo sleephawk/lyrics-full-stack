79e4329ff543f6bf9c29a989c8b45d5e3e9fb9b0
Georgy Chargaziya <georgy@Mac.lan>
2025-08-14 11:45:07 +0100

    addted delete and another get api calles

--- lyrics-api-springboot/src/main/java/com/example/demo/repositories/SongRepo.java
+++ lyrics-api-springboot/src/main/java/com/example/demo/repositories/SongRepo.java
@@ -2,9 +2,13 @@ package com.example.demo.repositories;
 
 import com.example.demo.models.Song;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface SongRepo extends JpaRepository<Song, Long> {
-
+    Optional<Song> findByName(String name);
 }
